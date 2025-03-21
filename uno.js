cc65595160c88391a85151c105ff20c68e9e590b
iamnotgracee <ejing124@gmail.com>
2025-03-21 17:13:59 +0800

    Update uno.js

--- uno.js
+++ uno.js
@@ -278,3 +278,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const sidebar = document.querySelector(".sidebar");
+
+    menuToggle.addEventListener("click", function () {
+        sidebar.classList.toggle("active");
+    });
+});
